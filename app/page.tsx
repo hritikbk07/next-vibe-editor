85945deb533f55a4fc9d321850cac636ad47f172
hritikbk07 <hritikkami56@gmail.com>
2026-05-12 01:14:07 +0545

    project setup

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen p-4">
+      <Button>
+        Get Started
+      </Button>
+    </div>
+      
+  );
+}
