a346b2de677ff2f29fcac55da512e61e5484fbf5
hritikbk07 <hritikkami56@gmail.com>
2026-05-21 23:57:42 +0545

    08-playground-file-explorer-1

---
+++ lib/template.ts
@@ -0,0 +1,8 @@
+export const templatePaths = {
+    REACT : "/vibecode-starter/react-ts",
+    VUE : "/vibecode-starter/vue",
+    ANGULAR : "/vibecode-starter/angular",
+    NEXTJS : "/vibecode-starter/nextjs-new",
+    EXPRESS : "/vibecode-starter/express-simple",
+    HONO : "/vibecode-starter/hono-nodejs-starter",    
+}
